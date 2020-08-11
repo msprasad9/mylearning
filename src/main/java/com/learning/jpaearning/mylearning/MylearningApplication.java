3053ba47c36f618289e9a0c481ff80c4ca762ecd
sivaprasad merikinapalli <msprasad9@gmail.com>
2020-08-11 18:44:07 -0400

    Java streams to sort and filter

--- src/main/java/com/learning/jpaearning/mylearning/MylearningApplication.java
+++ src/main/java/com/learning/jpaearning/mylearning/MylearningApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @SpringBootApplication
@@ -13,10 +14,12 @@ public class MylearningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MylearningApplication.class, args);
 		int[] numbers = {98, 11, 55, 50, 20, 40, 60, 80, 90, 2, 50};
+		List<Integer> myList = Arrays.asList(98, 1,5,566,77,1);
 		int target = 100;
 		HashMap<Integer,Integer> hash = new HashMap<>();
+		myList = myList.stream().sorted().filter(x -> x > 0).collect(Collectors.toList());
+		System.out.println("MyList:"+myList);
 		for(int i = 0; i < numbers.length; i++){
-
 			Integer diff = target - numbers[i];
 			if(hash.containsKey(diff)){
 				System.out.println("numbers:"+numbers[i]+","+diff);
